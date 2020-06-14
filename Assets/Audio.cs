a15f9c91408b4c9b281d556e10998a088da15e3d
DarkforcesII <darkforcesii@gmail.com>
2020-06-14 16:42:43 -0700

    Added Audio script

---
+++ Assets/Audio.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio : MonoBehaviour
+{
+    public AudioClip[] footSteps;
+    private AudioSource sfxSource;
+    bool hasPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sfxSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayFootSteps()
+    {
+        sfxSource.PlayOneShot(footSteps[Random.Range(0, footSteps.Length)]);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Terrain")
+        {
+            if (hasPlayed != true)
+            {
+                PlayFootSteps();
+                print("it works");
+                hasPlayed = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
